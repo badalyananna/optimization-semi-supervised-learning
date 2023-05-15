Corrected the gradient calculation, implemented Gauss Southwell rule for BCGD"

CyclicBCGD.py
@@ -1,42 +1,64 @@
 import time
 import numpy as np
-from common import calc_gradient, obj_func
+from common import calc_grad_y_j, calc_obj_func
 
-def cycl_BCGD(y_l, W_l, W_u, lr, num_blocks, max_iter, eps):
-    loss_stat = [] # loss statistics
-    time_stat = [] # time statistics
-    y_pred = np.zeros(len(W_u))
-    print(f"Initial loss: {obj_func(y_pred, y_l, W_l, W_u)}")
-
-    # calculate the partition size for unlabeled data
-    p_size_u = int(len(y_pred) / num_blocks)
+def cycl_BCGD(y_l, W_l, W_u, L_i, max_cycle, eps, calc_of=False, alpha_min=0):
+    """Implements the randomized BCGD method to minimize the objective function.
+    y_l - labeled variables
+    W_l - similarity matrix for labelled and unlabelled
+    W_u - similarity matrix for unlabelled
+    L_i - Lipschitz constant for each coordinate
+    alpha_min - sets the minimum value for the stepsize
+    max_cycle - maximum number of cycles
+    eps - tolerance
+    calc_of - boolean variable, if True the objective functions is calucated after each cycle
+    """
+    start_time = time.time()
+      
+    time_stat = [0] 
+    grad_stat = []
+    obj_fun_stat = []
     
-    for iter in range(max_iter):
-        y_old = y_pred.copy()
-        grad_time = 0
-        for i in range(num_blocks):
-            # slice the blocks
-            y_k = y_pred[(i*p_size_u):((i+1)*p_size_u)]
-            W_l_k = W_l[:, (i*p_size_u):((i+1)*p_size_u)]
-            W_u_k = W_u[(i*p_size_u):((i+1)*p_size_u), (i*p_size_u):((i+1)*p_size_u)]
+    n = len(W_u)
+    y_pred = np.zeros(n)
+    alpha = 1 / L_i
+        
+    obj_fun = calc_obj_func(y_pred, y_l, W_l, W_u)
+    print(f"Initial loss: {obj_fun}")
 
+    for it in range(max_cycle):
+        stop_cond = 0
+        for jk in range(n):
             # calculate the gradient
-            start = time.time()
-            grad = calc_gradient(y_k, y_l, W_l_k, W_u_k)
-            end = time.time()
-            grad_time += end - start
+            grad = calc_grad_y_j(jk, y_pred, y_l, W_l, W_u)
+            direction = - grad
 
             # update predictions
-            y_k -= lr * grad
-            y_pred[(i*p_size_u):((i+1)*p_size_u)] = y_k
+            alpha_k = max(alpha[jk], alpha_min)
+            u = alpha_k * direction
+            y_j += u
+            y_pred[jk] = y_j
         
-        time_stat.append(grad_time)
-        loss = obj_func(y_pred, y_l, W_l, W_u)
-        loss_stat.append(loss)
-
-        stop_cond = np.linalg.norm(y_old - y_pred)
-        print(f"Interation {iter}, loss:  {obj_func(y_pred, y_l, W_l, W_u)}, norm: {stop_cond}")
+            stop_cond += abs(grad*direction)
+            
+        stop_cond /= n # take the average of gradient and direction products
+        grad_stat.append(stop_cond)
+        iter_time = time.time()
+        time_stat.append(iter_time-start_time)
         if stop_cond < eps:
-          break
-
-    return y_pred, loss_stat, time_stat 
+            obj_fun = calc_obj_func(y_pred, y_l, W_l, W_u)
+            obj_fun_stat.append(obj_fun)
+            print(f"""
+            Stopping condition satisfied, obj fun: {obj_fun}, 
+            Gradient norm: {stop_cond}",
+            Total CPU time: {iter_time-start_time}""")
+            break
+        
+        if calc_of:
+            obj_fun_val = calc_obj_func(y_pred, y_l, W_l, W_u)
+            obj_fun_stat.append(obj_fun_val)
+            print(f"Cycle {it+1}: obj fun {obj_fun_val}, gradient norm {stop_cond}")
+        else:
+            print(f"Cycle {it+1}: gradient norm {stop_cond}")
+     
+    return y_pred, grad_stat, time_stat, obj_fun_stat

DataPreparation.py
@@ -50,15 +50,15 @@ def prepare_data(path):
     lr = config.get('learningrate')
     max_iter = config.get('maxiterations')
     eps = config.get('epsilon')
-    randomseed = config.get('randomseed')
+    random_seed = config.get('randomseed')
 
 
-    X, y = make_blobs(n_samples=noofsamples, centers=noofcenters, n_features=nooffeatures, random_state=123)
+    X, y = make_blobs(n_samples=noofsamples, centers=noofcenters, n_features=nooffeatures, random_state=random_seed)
 
     y[y == 0] = -1
 
     # first we shuffle and build the similarity matrix in order to separate later without shuffling and keep the form of the W matrix
-    X, y = shuffle(X, y, random_state=1)
+    X, y = shuffle(X, y, random_state=random_seed)
     # W = 1 / (pairwise_distances(X) + 0.0001)
     W  = np.exp(-(euclidean_distances(X, squared=True)) * 0.5)
     X_l, X_u, y_l, y_u , W_l, W_u = train_test_split(X, y, W, test_size = testsize, shuffle=False)
@@ -68,5 +68,5 @@ def prepare_data(path):
     W_l = W_l[:,n:]
     W_u = W_u[:,n:]
 
-    return X,y,X_l,X_u,y_l,y_u,W_l,W_u,lr,max_iter,eps,randomseed
+    return X,y,X_l,X_u,y_l,y_u,W_l,W_u,lr,max_iter,eps,random_seed
 

GradientDescent.py
@@ -1,25 +1,54 @@
 import time
 import numpy as np
-from common import calc_gradient, obj_func
+from common import calc_grad_full, calc_obj_func
 
+def GM(y_l, W_l, W_u, alpha, max_iter, eps, calc_of=False):
+    """Algorithms that implements gradient method.
+    y_l - labeled variables
+    W_l - similarity matrix for labelled and unlabelled
+    W_u - similarity matrix for unlabelled
+    alpha - fixed step size
+    max-iter - maximum number of iteration
+    eps - tolerance
+    calc_of - boolean variable, if True the objective functions is calucated at each iteration
+    """
+    start_time = time.time()
+    grad_stat = []
+    obj_fun_stat = []
+    time_stat = [0]
+    
+    n = len(W_u)
+    y_pred = np.zeros(n)
 
-def GM_algorithm(y_l, W_l, W_u, lr, max_iter, eps):
-    loss_stat = [] # loss statistics
-    time_stat = [] # time statistics
-    y_pred = np.zeros(len(W_u))
-    print(f"Initial loss: {obj_func(y_pred, y_l, W_l, W_u)}")
+        
+    obj_fun_val = calc_obj_func(y_pred, y_l, W_l, W_u)
+    obj_fun_stat.append(obj_fun_val)
+    print(f"Initial loss: {obj_fun_val}")
+    
     for i in range(max_iter):
-        start_grad = time.time()
-        gradient = calc_gradient(y_pred, y_l, W_l, W_u)
-        end_grad = time.time()
-        time_stat.append(end_grad - start_grad)
+        gradient = calc_grad_full(y_pred, y_l, W_l, W_u)
+        direction = - gradient
 
-        y_pred += -lr * gradient
+        y_pred += alpha * direction
 
-        stop_cond = np.linalg.norm(gradient) * abs(lr)
-        loss = obj_func(y_pred, y_l, W_l, W_u)
-        loss_stat.append(loss)
-        print(f"Iteration {i+1}: loss {loss}, gradient norm {stop_cond}")
+        stop_cond = abs(direction @ gradient) / n
+        
+        if calc_of:
+            obj_fun_val = calc_obj_func(y_pred, y_l, W_l, W_u)
+            obj_fun_stat.append(obj_fun_val)
+            print(f"Iteration {i+1}: obj fun {obj_fun_val}, gradient norm {stop_cond}")
+        else:
+            print(f"Iteration {i+1}: gradient norm {stop_cond}")
+            
+        iter_time = time.time()
+        time_stat.append(iter_time - start_time)
+        grad_stat.append(stop_cond)
         if stop_cond < eps:
+            obj_fun = calc_obj_func(y_pred, y_l, W_l, W_u)
+            obj_fun_stat.append(obj_fun)
+            print(f"""
+            Stopping condition satisfied, obj fun: {obj_fun}, 
+            Gradient norm: {stop_cond}",
+            Total CPU time: {iter_time-start_time}""")
             break
-    return y_pred, loss_stat, time_stat
+    return y_pred, grad_stat, time_stat, obj_fun_stat

RandomBCGD.py
@@ -1,45 +1,76 @@
 import time
 import numpy as np
-from common import calc_gradient, obj_func
+from common import calc_grad_y_j, calc_obj_func
 
-def rand_BCGD(y_l, W_l, W_u, lr, num_blocks, max_iter, eps, random_seed=None):
+def rand_BCGD(y_l, W_l, W_u, L_i, max_cycle, eps, nesterov_sampling=False, calc_of=False, alpha_min=0, random_seed=None):
+    """Implements the randomized BCGD method to minimize the objective function.
+    y_l - labeled variables
+    W_l - similarity matrix for labelled and unlabelled
+    W_u - similarity matrix for unlabelled
+    L_i - Lipschitz constant for each coordinate
+    alpha_min - sets the minimum value for the stepsize
+    max_cycle - maximum number of cycles
+    eps - tolerance
+    nesterov_samling - if true, samples the coordinates according to the Nesterov's rule,
+        choosing the coordinates with the largest L_i more often
+    calc_of - boolean variable, if True the objective functions is calucated after each cycle
+    """
+    start_time = time.time()
     np.random.seed(random_seed)
-    loss_stat = [] # loss statistics
-    time_stat = [] # time statistics
-    y_pred = np.zeros(len(W_u))
-    print(f"Initial loss: {obj_func(y_pred, y_l, W_l, W_u)}")
-
-    # calculate the partition size for unlabeled data
-    p_size_u = int(len(y_pred) / num_blocks)
     
-    for iter in range(max_iter):
-        # recoding the copy of old values in order to use the change as a stopping condition
-        y_old = y_pred.copy()
-        grad_time = 0
-        for i in range(num_blocks):
-            # choose random block
-            ik = np.random.randint(0, num_blocks)
-            # slice the blocks
-            y_k = y_pred[(ik*p_size_u):((ik+1)*p_size_u)]
-            W_l_k = W_l[:, (ik*p_size_u):((ik+1)*p_size_u)]
-            W_u_k = W_u[(ik*p_size_u):((ik+1)*p_size_u), (ik*p_size_u):((ik+1)*p_size_u)]
+    time_stat = [0] 
+    grad_stat = []
+    obj_fun_stat = []
+    
+    n = len(W_u)
+    y_pred = np.zeros(n)
+    alpha = 1 / L_i
+    if nesterov_sampling:
+        p = L_i / L_i.sum()
+        
+    obj_fun = calc_obj_func(y_pred, y_l, W_l, W_u)
+    print(f"Initial loss: {obj_fun}")
+
+    for it in range(max_cycle):
+        stop_cond = 0
+        for _ in range(n):
+            # choose random coordinate
+            if nesterov_sampling:
+                jk = np.random.choice(n, p=p)
+            else:
+                jk = np.random.randint(n)
+            y_j = y_pred[jk]
 
             # calculate the gradient
-            start = time.time()
-            grad = calc_gradient(y_k, y_l, W_l_k, W_u_k)
-            end = time.time()
-            grad_time += end - start
+            grad = calc_grad_y_j(jk, y_pred, y_l, W_l, W_u)
+            direction = - grad
 
             # update predictions
-            y_k -= lr * grad
-            y_pred[(ik*p_size_u):((ik+1)*p_size_u)] = y_k
-
-        time_stat.append(grad_time)
-        stop_cond = np.linalg.norm(y_old - y_pred)
-        loss = obj_func(y_pred, y_l, W_l, W_u)
-        loss_stat.append(loss)
-        print(f"Interation {iter}, loss:  {loss}, norm: {stop_cond}")
+            alpha_k = max(alpha[jk], alpha_min)
+            u = alpha_k * direction
+            y_j += u
+            y_pred[jk] = y_j
+        
+            stop_cond += abs(grad*direction)
+            
+        stop_cond /= n # take the average of gradient and direction products
+        grad_stat.append(stop_cond)
+        iter_time = time.time()
+        time_stat.append(iter_time-start_time)
         if stop_cond < eps:
-          break
-
-    return y_pred, loss_stat, time_stat
+            obj_fun = calc_obj_func(y_pred, y_l, W_l, W_u)
+            obj_fun_stat.append(obj_fun)
+            print(f"""
+            Stopping condition satisfied, obj fun: {obj_fun}, 
+            Gradient norm: {stop_cond}",
+            Total CPU time: {iter_time-start_time}""")
+            break
+        
+        if calc_of:
+            obj_fun_val = calc_obj_func(y_pred, y_l, W_l, W_u)
+            obj_fun_stat.append(obj_fun_val)
+            print(f"Cycle {it+1}: obj fun {obj_fun_val}, gradient norm {stop_cond}")
+        else:
+            print(f"Cycle {it+1}: gradient norm {stop_cond}")
+     
+    return y_pred, grad_stat, time_stat, obj_fun_stat

common.py
@@ -1,13 +1,54 @@
 import yaml
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import linalg as LA
 
 def read_config(config_path):
     with open(config_path) as config_file:
         content = yaml.safe_load(config_file)
     return content
 
-def calc_gradient(y, y_l, W_l, W_u):
+def plot_data(X,Y):
+    plt.scatter(X.T[0], X.T[1], c=Y)
+    plt.show()
+
+def eigval_hessian(W_l, W_u):
+    """Returns the largest eigenvalue of the Hessian matrix.
+    """
+    n = len(W_u)
+    hes = W_u * (-2)
+    diag_values = (W_l.sum(axis=0) + W_u.sum(axis=0)) * 2
+    np.fill_diagonal(hes, diag_values)
+    max_eig = LA.eigh(hes, eigvals_only=True, subset_by_index=[n-1, n-1])[0]
+    return max_eig
+
+def calc_obj_func(y, y_l, W_l, W_u):
+    """Calculates the value of the objective function.
+    y - unlabelled predicted variables
+    y_l - labeled variables
+    W_l - similarity matrix for labelled and unlabelled
+    W_u - similarity matrix for unlabelled
+    """
+    nl = len(y_l) # number of labeled points
+    nu = len(y) # number of unlabeled points
+    part1 = np.outer(np.ones(nl), y) - np.outer(y_l, np.ones(nu))
+    part1 = np.multiply(part1, part1)
+    part1 = np.multiply(W_l, part1)
+
+    part2 = np.outer(np.ones(nu), y) - np.outer(y, np.ones(nu))
+    part2 = np.multiply(part2, part2)
+    part2 = np.multiply(W_u, part2)
+
+    return np.sum(part1) + (0.5 * np.sum(part2))
+
+def calc_grad_full(y, y_l, W_l, W_u):
+    """Calculated the gradient with respect to y
+    Outputs a vector of dimensions equal to y 
+    y - unlabelled predicted variables
+    y_l - labeled variables
+    W_l - similarity matrix for labelled and unlabelled
+    W_u - similarity matrix for unlabelled
+    """
     nl = len(y_l) # number of labeled points
     nu = len(y) # number of unlabeled points
     part1 = np.outer(np.ones(nl), y) - np.outer(y_l, np.ones(nu))
@@ -19,100 +60,36 @@ def calc_gradient(y, y_l, W_l, W_u):
     part2 = np.matmul(part2.T, np.ones(nu))
     return 2 * (part1 + part2)
 
-def obj_func(y, y_l, W_l, W_u):
-    nl = len(y_l) # number of labeled points
-    nu = len(y) # number of unlabeled points
-    part1 = np.outer(np.ones(nl), y) - np.outer(y_l, np.ones(nu))
-    part1 = np.multiply(part1, part1)
-    part1 = np.multiply(W_l, part1)
-
-    part2 = np.outer(np.ones(nu), y) - np.outer(y, np.ones(nu))
-    part2 = np.multiply(part2, part2)
-    part2 = np.multiply(W_u, part2)
-    return np.sum(part1) + (0.5 * np.sum(part2))
-
-def power_method(A, n):
-    """Calculates the largest eigenvalue and corresponding eigenvector using the power method.
-    A - input matrix
-    n - the number of iterations of the power method
+def calc_grad_y_j(j, y, y_l, W_l, W_u):
+    """Calculated the gradient with respec to y_j
+    j - the coordinate with respect to which the gradient is calculated
+    y - unlabelled predicted variables
+    y_l - labeled variables
+    W_l - similarity matrix for labelled and unlabelled points
+    W_u - similarity matrix for unlabelled points
     """
-    x = np.ones(len(A[0]))
-    for _ in range(n):
-        y = np.matmul(A, x)
-        eigenvalue = np.linalg.norm(y)
-        eigenvector = y / np.linalg.norm(y)
-    return eigenvector, eigenvalue
+    W_l_j = W_l[:,j]
+    W_u_j = W_u[:,j]
 
-def calc_Lipshitz(W_l, W_u):
-    hes = W_u * (-2)
-    diag_values = (W_l.sum(axis=0) + W_u.sum(axis=0))
-    np.fill_diagonal(hes, diag_values)
-    vec, L = power_method(hes, 500)
-    return L
+    part1 = np.dot(W_l_j, y[j]-y_l)
+    part2 = np.dot(W_u_j, y[j] - y)
+    return 2 * (part1 + part2)
 
-def plot_data(X,Y):
-    plt.scatter(X.T[0], X.T[1], c=Y)
-    plt.show()
+def update_gradient(ik, update, old_grad, W_u):
+    """Updates the value of the gradient given the change in one coordinate
+    ik - the coordinate that changed
+    update - the value of the change
+    old_grad - old value of the gradient
+    W_u -similarity matrix for unlabelled points
+    """
+    return old_grad + 2 * W_u[ik] * update
 
-def graph1(loss_stat_GM, loss_stat_RB,loss_stat_CB):
-    # plotting loss and gradient computation time
-    # for BCGD the time for each gradient calculation in the number of blocks was summed
-    plt.figure(figsize=(16,7))
-    plt.subplot(1, 2, 1)
-    plt.plot(loss_stat_GM, label = "Gradient Method")
-    plt.plot(loss_stat_RB, label = "Randomized BCGD")
-    plt.plot(loss_stat_CB, label = "Cyclic BCGD")
-    plt.title("Loss at each iteration")
-    plt.xlabel("Iteration")
-    plt.ylabel("Loss")
-    plt.legend()
-
-    # plot the loss after n iterations
-    n = 75
-    plt.subplot(1, 2, 2)
-    plt.plot(list(range(len(loss_stat_GM)))[n:], loss_stat_GM[n:], label = "Gradient Method")
-    plt.plot(list(range(len(loss_stat_RB)))[n:], loss_stat_RB[n:], label = "Randomized BCGD")
-    plt.plot(list(range(len(loss_stat_CB)))[n:], loss_stat_CB[n:], label = "Cyclic BCGD")
-    plt.title(f"Loss after {n} iterations")
-    plt.xlabel("Iteration")
-    plt.ylabel("Loss")
-    plt.legend()
-
-    plt.show()
-
-def graph2(time_stat_GM, time_stat_RB, time_stat_CB):
-    plt.figure(figsize=(8,7))
-    plt.plot(time_stat_GM, label = "Gradient Method")
-    plt.plot(time_stat_RB, label = "Randomized BCGD")
-    plt.plot(time_stat_CB, label = "Cyclic BCGD")
-    plt.title("Gradient computation time per iteration")
-    plt.xlabel("Iteration")
-    plt.ylabel("CPU time")
-    plt.legend()
-    plt.show()
-
-def graph3(time_stat_GM, time_stat_RB, time_stat_CB):
-    # total and average gradient computation time
-    total_GM = np.array(time_stat_GM).sum()
-    total_RB = np.array(time_stat_RB).sum()
-    total_CB = np.array(time_stat_CB).sum()
-
-    avg_per_iter_GM = total_GM / len(time_stat_GM)
-    avg_per_iter_RB = total_RB / len(time_stat_RB)
-    avg_per_iter_CB = total_CB / len(time_stat_CB)
-
-    x_ticks = np.array(['GM', 'Rand BCGD', 'Cycl BCGD'])
-    total_time = np.array([total_GM, total_RB, total_CB])
-    avg_time = np.array([avg_per_iter_GM, avg_per_iter_RB, avg_per_iter_CB])
-
-    plt.figure(figsize=(12,5))
-    plt.subplot(1, 2, 1)
-    plt.bar(x_ticks,total_time)
-    plt.title("Total time for gradient computation")
-
-    plt.subplot(1, 2, 2)
-    plt.bar(x_ticks,avg_time)
-    plt.title("Average time per iteration")
-    plt.show()
-
-
+def update_obj_function(jk, y_pred, y_l, W_l, W_u, u):
+    """Updates the value of the obbjective function given the change in one coordinate
+    jk - the coordinate that changed
+    u - update, the value of the change
+    """
+    diff = (2 * (y_pred[jk] - y_l) - u) @ W_l[:, jk] 
+    diff -= 0.5 * (2 * (y_pred - y_pred[jk]) + u) @ W_u[jk]
+    diff += 0.5 * (2 * (y_pred[jk] - y_pred) - u) @ W_u[:,jk]
+    return u * diff 